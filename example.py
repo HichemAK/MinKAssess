1edb842fda0423d55a39f4af758ff581f118ac01
Hichem Ammar Khodja <hichem.ammarkhodja@orange.com>
2024-10-26 10:21:28 +0200

    black reformating

--- example.py
+++ example.py
@@ -1,8 +1,8 @@
 from minkarr import KaRR
 from transformers import AutoModelForCausalLM, AutoTokenizer
 
-model_name = 'gpt2'
-device = 'cuda'
+model_name = "gpt2"
+device = "cuda"
 model = AutoModelForCausalLM.from_pretrained(model_name).cuda()
 tokenizer = AutoTokenizer.from_pretrained(model_name)
 
@@ -10,10 +10,10 @@ karr = KaRR(model, tokenizer, device)
 
 # Testing the fact: (France, capital, Paris)
 # You can find other facts by looking into Wikidata
-fact = ('Q142', 'P36', 'Q90')
+fact = ("Q142", "P36", "Q90")
 
 karr, does_know = karr.compute(fact)
-print('Fact %s' % str(fact))
-print('KaRR = %s' % karr)
-ans = 'Yes' if does_know else 'No'
-print('According to KaRR, does the model knows this fact? Answer: %s' % ans)
+print("Fact %s" % str(fact))
+print("KaRR = %s" % karr)
+ans = "Yes" if does_know else "No"
+print("According to KaRR, does the model knows this fact? Answer: %s" % ans)
--- src/minkarr/utils.py
+++ src/minkarr/utils.py
@@ -9,23 +9,26 @@ from requests.adapters import HTTPAdapter
 
 
 def load_json(path) -> dict:
-    with open(path, 'r') as f:
+    with open(path, "r") as f:
         return json.load(f)
-    
+
+
 class DownloadError(Exception):
     """Custom exception for download errors."""
+
     pass
-    
+
+
 def download_and_unzip(url, extract_to, retries=3, backoff_factor=0.3):
     def is_zip_file(response, url):
         """
         Determine if the response is a zip file either by the Content-Disposition header
         or by the file extension in the URL.
         """
-        content_disposition = response.headers.get('Content-Disposition', '')
-        if 'zip' in content_disposition:
+        content_disposition = response.headers.get("Content-Disposition", "")
+        if "zip" in content_disposition:
             return True
-        if url.lower().endswith('.zip'):
+        if url.lower().endswith(".zip"):
             return True
         return False
 
@@ -39,8 +42,8 @@ def download_and_unzip(url, extract_to, retries=3, backoff_factor=0.3):
         # method_whitelist=["HEAD", "GET", "OPTIONS"]
     )
     # Mount it for both http and https usage
-    session.mount('http://', HTTPAdapter(max_retries=retry_strategy))
-    session.mount('https://', HTTPAdapter(max_retries=retry_strategy))
+    session.mount("http://", HTTPAdapter(max_retries=retry_strategy))
+    session.mount("https://", HTTPAdapter(max_retries=retry_strategy))
 
     try:
         # Send a HTTP request to the URL
@@ -48,22 +51,24 @@ def download_and_unzip(url, extract_to, retries=3, backoff_factor=0.3):
         with session.get(url, stream=True) as response:
             # Raise an exception if the download failed
             response.raise_for_status()
-            
+
             # Check if the response content is a zip file
             if is_zip_file(response, url):
                 # Create a BytesIO object to hold the chunks of data
                 zip_file_bytes = BytesIO()
-                total_size = int(response.headers.get('content-length', 0))
-                with tqdm(total=total_size, unit='B', unit_scale=True, desc="Downloading") as progress_bar:
+                total_size = int(response.headers.get("content-length", 0))
+                with tqdm(
+                    total=total_size, unit="B", unit_scale=True, desc="Downloading"
+                ) as progress_bar:
                     for chunk in response.iter_content(chunk_size=8192):
                         # Filter out keep-alive new chunks
                         if chunk:
                             zip_file_bytes.write(chunk)
                             progress_bar.update(len(chunk))
-                
+
                 # Extract the zip file
                 zip_file_bytes.seek(0)  # Move to the beginning of the BytesIO object
-                with zipfile.ZipFile(zip_file_bytes, 'r') as zip_ref:
+                with zipfile.ZipFile(zip_file_bytes, "r") as zip_ref:
                     # Extract the zip file to the specified directory
                     print(f"Extracting files to '{extract_to}' folder")
                     zip_ref.extractall(extract_to)
